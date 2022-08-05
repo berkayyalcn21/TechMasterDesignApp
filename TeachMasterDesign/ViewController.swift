9f6a35713e28f60452962f1c1899fa21c6b62454
Ahmet Berkay Yalçın <ahmetberkay.yalcin@pru.edu.tr>
2022-08-05 18:39:13 +0300

    Upload

---
+++ TeachMasterDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeachMasterDesign
+//
+//  Created by Berkay on 5.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Status bar white mode
+        navigationController?.navigationBar.barStyle = .black
+    }
+    
+}
+
